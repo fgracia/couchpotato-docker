38c6460e729002838b892daf0e14b0df290d998c
Frédéric GRACIA <fgracia@sqli.com>
2017-02-21 22:48:36 +0100

    Added T411 plugin

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-git clone https://github.com/cocazoulou/couchpotato.provider.t411.git tmp
+if [ -d tmp ] ; then
+  rm -rf tmp
+fi
 
+git clone https://github.com/cocazoulou/couchpotato.provider.t411.git tmp
 docker build -t fgracia/couchpotato .
 
 rm -rf tmp
