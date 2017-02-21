e40bfe808529eda20362f0e9e1879d0097fdd7b3
Frédéric GRACIA <fgracia@sqli.com>
2017-02-21 22:46:51 +0100

    Added plugin T411

--- startup.sh
+++ startup.sh
@@ -5,32 +5,35 @@ if [ -f /data/settings.conf ]
 then
 	rm -f /root/.couchpotato/settings.conf
 	ln -s /data/settings.conf /root/.couchpotato/settings.conf
-fi 
+fi
 
 if [ -d /data/logs ]
 then
 	rm -rf /root/.couchpotato/logs
 	ln -s /data/logs /root/.couchpotato/logs
-fi 
+fi
 
 if [ -d /data/database ]
 then
 	rm -rf /root/.couchpotato/database
 	ln -s /data/database /root/.couchpotato/database
-fi 
+fi
 
 if [ -d /data/custom_plugins ]
 then
 	rm -rf /root/.couchpotato/custom_plugins
 	ln -s /data/custom_plugins /root/.couchpotato/custom_plugins
-fi 
+else
+	mkdir /root/.couchpotato/custom_plugins
+fi
 
 if [ -d /data/cache ]
 then
 	rm -rf /root/.couchpotato/cache
 	ln -s /data/cache /root/.couchpotato/cache
-fi 
+fi
 
+cp -r /root/custom_plugins/t411 /root/.couchpotato/custom_plugins/t411
 
 # Running CouchPotato
 /usr/bin/python /opt/CouchPotatoServer/CouchPotato.py
